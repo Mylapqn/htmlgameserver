46d7bf889aed9d1c9663ef428c10eb1e0fe508c4
Matouš Marek <mylapqn312@gmail.com>
2019-11-20 23:29:41 +0100

    join and leave messages.

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ wsServer = new WebSocketServer({
 
 var userCount = 0;
 
+function sendAll(s){
+  for (var i=0; i<users.length;i++){
+    users[i].sendUTF(s);
+  }
+}
+
 // WebSocket server
 wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
@@ -32,6 +38,9 @@ wsServer.on('request', function(request) {
   users.push(connection);
   console.log((new Date()) + ' Connection accepted. UserID = ' + userID);
   console.log(" new users: "+users.length);
+ 
+    sendAll(JSON.stringify({type:"message", data: "User " + userID + "Has joined the chat. "}));
+
   userCount++;
   connection.sendUTF("ConnectionServerTest");
   connection.sendUTF(JSON.stringify({type:"technical", subtype:"userID", data: userID}));
@@ -41,9 +50,9 @@ wsServer.on('request', function(request) {
   connection.on('message', function(message) {
     if (message.type === 'utf8') {
         console.log((new Date()) + ' New message: ' + message.utf8Data);
-        for (var i=0; i<users.length;i++){
-            users[i].sendUTF(JSON.stringify({type:"message", data: "User " + userID + ": " + message.utf8Data}));
-        }
+        
+          sendAll(JSON.stringify({type:"message", data: "User " + userID + ": " + message.utf8Data}));
+        
     }
   });
 
@@ -51,6 +60,8 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection closed. UserID = ' + userID);
     userCount--;
     users.splice(userID, 1);
+    sendAll(JSON.stringify({type:"message", data: "User " + userID + "Has left the chat. "}));
+
     console.log("remaining users: "+users.length);
   });
 });
