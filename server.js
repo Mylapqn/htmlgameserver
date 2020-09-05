c8cf0b03ab04b79582340de3dc2086b8c7ca2050
Matouš Marek <mylapqn312@gmail.com>
2020-09-05 23:22:22 +0200

    serialization

--- server.js
+++ server.js
@@ -310,7 +310,8 @@ function writeBufferUInt8(buffer, position, value) {
   return 1;
 }
 function writeBufferUInt16(buffer, position, value) {
-  let bytesInt = new DataView(buffer, position);
+  console.log("FUKIJERFW: " + position + " " + buffer.byteLength);
+  let bytesInt = new DataView(buffer, position,2);
   bytesInt.setUint16(0, value);
   return 2;
 }
